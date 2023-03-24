0bc3e424de0d6dc53274233842a4a8957faa4019
haggau15 <haggau94@gmail.com>
2023-03-24 10:25:51 +0100

    Added recursive implementation of factorial

--- src/Recursion/Fibonacci.java
+++ src/Recursion/Fibonacci.java
@@ -3,22 +3,33 @@ package Recursion;
 public class Fibonacci {
     public static void main(String[] args)
     {
-        for (int i =0;i<40;i++)
+        int n=7;
+        for (int i =0;i<1;i++)
         {
-            System.out.print(fibonacci(i)+" ");
+            //System.out.print(fibonacci(i)+" ");
         }
+        System.out.println("Factorial of "+n+": "+factorial(n));
     }
     public static int fibonacci(int n)
     {
-        if (n==0)
+        if (n<=0)
         {
             return 0;
         }
-         if (n==1 || n==2){
+         else if (n==1 || n==2){
              return 1;
          }    else {
              return (fibonacci(n-1)+fibonacci(n-2));
          }
+    }
+    public static int factorial(int n)
+    {
+        if (n==1)
+        {
+            return 1;
+        } else {
+            return (n*factorial(n-1));
+        }
     }
 
 }
