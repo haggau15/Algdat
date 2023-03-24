91d0c25a28aa7d14c0f1b80b0cc8cef3980a0dcc
haggau15 <haggau94@gmail.com>
2023-03-24 10:13:22 +0100

    Added recursive implementation of the fibonacci sequence

--- src/Recursion/Fibonacci.java
+++ src/Recursion/Fibonacci.java
@@ -1,4 +1,24 @@
 package Recursion;
 
 public class Fibonacci {
+    public static void main(String[] args)
+    {
+        for (int i =0;i<40;i++)
+        {
+            System.out.print(fibonacci(i)+" ");
+        }
+    }
+    public static int fibonacci(int n)
+    {
+        if (n==0)
+        {
+            return 0;
+        }
+         if (n==1 || n==2){
+             return 1;
+         }    else {
+             return (fibonacci(n-1)+fibonacci(n-2));
+         }
+    }
+
 }
